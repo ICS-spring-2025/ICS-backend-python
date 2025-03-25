9e4be26271a9bbc7389cd5433df2e4974edfbfa8
larentina <118663828+larentiina@users.noreply.github.com>
2025-03-25 16:37:26 +0300

    add exception for non exists event

--- structure.py
+++ structure.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass
 
-dumb_memory_path = "."
+dumb_memory_path = "./trace_dump.bin"
 
 @dataclass
 class Event:
     id: int
     name: str
-    timestamps: list[float]
-    data: int
+    records: list[tuple[float, int]]
     
 store: dict[int, Event] = {}
+
+class EventNotFoundError(Exception):
+    pass
 
 name_events = {
     1: "blocking_on_queue_peek",
@@ -92,13 +94,18 @@ name_events = {
 }
 
 
-def event(id: int, timestamps: list[float], data: int) -> Event:
+def event(id: int, timestamp: float, data: int) -> Event:
+    name = name_events.get(id)
+    if name is None:
+        raise EventNotFoundError(f"Event with id {id} not found in name_events")
+    
     return Event(
         id=id,
-        name=name_events.get(id),
-        timestamps=timestamps,
-        data=data
-    ) 
-
-
-
+        name=name,
+        records=[(timestamp, data)]
+    )
+    
+def add_record_to_event(id: int, timestamp:float, data: int):
+    event = store.get(id)
+    event.records.append((timestamp, data))
+    store[id] = event
